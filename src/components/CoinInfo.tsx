e592b738745027e13dd27f65e57e2aae87ecc3f9
Tyler Cole <47987246+tcole8899@users.noreply.github.com>
2021-04-01 16:21:19 -0400

    added ability to load extra coins and a loading animation

--- src/components/CoinInfo.tsx
+++ src/components/CoinInfo.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import Loader from './Loading';
 
 const CoinTable = styled.table`
     width: 100%;
@@ -7,6 +8,7 @@ const CoinTable = styled.table`
 `;
 
 const Row = styled.tr`
+    height: 30px;
     background: white;
     &:nth-child(even) {
         background: #dedede
@@ -14,6 +16,10 @@ const Row = styled.tr`
     font-size: 15px;
     &:first-child {
         font-size: 20px;
+        height: 40px;
+    }
+    &:last-child {
+        height: 40px;
     }
 `;
 
@@ -27,6 +33,7 @@ const Filter = styled.a`
 `;
 
 interface Response {
+    id: string,
     name: string,
     symbol: string,
     rank: number,
@@ -59,27 +66,56 @@ function RenderCoinRow(props: RenderProps){
 
 export default function CoinInfo(){
     const [error, setError] = useState(null);
+    const [startPoint, setStartPoint] = useState(0);
     const [isLoaded, setIsLoaded] = useState(false);
-    const [coinData, setCoinData] = useState<{data: Response[], info: {}}>({ data: [], info: {} });
-    const [sortOrder, setSortOrder] = useState({name: 0, symbol: 0, rank: 0, price_usd: 0, percent_change_1h: 0, percent_change_24h: 0, percent_change_7d: 0});
+    const [isLoading, setIsLoading] = useState(true);
+    const [coinData, setCoinData] = useState<{data: Response[], info: {}}>({ 
+        data: [], 
+        info: {} 
+    });
+    const [sortOrder, setSortOrder] = useState({
+        name: 0, 
+        symbol: 0, 
+        rank: 1, 
+        price_usd: 0, 
+        percent_change_1h: 0, 
+        percent_change_24h: 0, 
+        percent_change_7d: 0
+    });
   
     useEffect(() => {
-      fetch("https://api.coinlore.net/api/tickers/?start=0&limit=100")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setCoinData(result);
-            setIsLoaded(true);
-            console.log(result);
-          },
-          // Check for errors
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-            console.log(error);
-          }
+        let start = startPoint;
+        let API_CALL = ['https://api.coinlore.net/api/tickers/?start=', start.toString(), '&limit=100'];
+        fetch("".concat(...API_CALL))
+            .then(res => res.json())
+            .then(
+            (result) => {
+                if (start === 0) {
+                    setCoinData(result);
+                    setIsLoaded(true);
+                    setIsLoading(false);
+                }
+                else {
+                    setCoinData((priorCoinData) => {
+                        const newCoinData = priorCoinData.data.concat(result.data);
+                        return ({
+                            data: newCoinData,
+                            info: priorCoinData.info
+                        })
+                    })
+                    setIsLoaded(true);
+                    setIsLoading(false);
+                }
+            },
+            // Check for errors
+            (error) => {
+                setIsLoaded(true);
+                setIsLoading(false);
+                setError(error);
+                console.log(error);
+            }
         )
-    },[])
+    },[startPoint])
 
     function sortBy(field: string){
         let orderBy: number;
@@ -129,28 +165,35 @@ export default function CoinInfo(){
     else if (isLoaded && error === null) {
         return(
             <CoinTable>
-                <Row>
-                    <td><Filter onClick={() => sortBy("name")}>Coin</Filter></td>
-                    <td><Filter onClick={() => sortBy("symbol")}>Symbol</Filter></td>
-                    <td><Filter onClick={() => sortBy("rank")}>Rank</Filter></td>
-                    <td><Filter onClick={() => sortBy("price_usd")}>Price</Filter></td>
-                    <td><Filter onClick={() => sortBy("percent_change_1h")}>% Change - 1hr</Filter></td>
-                    <td><Filter onClick={() => sortBy("percent_change_24h")}>% Change - 24hr</Filter></td>
-                    <td><Filter onClick={() => sortBy("percent_change_7d")}>% Change - 7day</Filter></td>
-                </Row>
-                {
-                coinData.data.map((coin, index) => {
-                    return(
-                        <RenderCoinRow data={coin}/>
-                    )
-                })
-                }
+                <tbody>
+                    <Row>
+                        <td><Filter onClick={() => sortBy("name")}>Coin</Filter></td>
+                        <td><Filter onClick={() => sortBy("symbol")}>Symbol</Filter></td>
+                        <td><Filter onClick={() => sortBy("rank")}>Rank</Filter></td>
+                        <td><Filter onClick={() => sortBy("price_usd")}>Price</Filter></td>
+                        <td><Filter onClick={() => sortBy("percent_change_1h")}>% Change - 1hr</Filter></td>
+                        <td><Filter onClick={() => sortBy("percent_change_24h")}>% Change - 24hr</Filter></td>
+                        <td><Filter onClick={() => sortBy("percent_change_7d")}>% Change - 7day</Filter></td>
+                    </Row>
+                    {coinData.data.map(coin => {
+                        return(
+                            <RenderCoinRow data={coin} key={coin.id}/>
+                        )
+                    })}
+                    <Row>
+                        <th colSpan={7}>
+                            <button onClick={() => { setStartPoint(startPoint + 100);
+                                                     setIsLoading(true); }}>
+                            Show Next 100 Coins
+                            </button>
+                        </th>
+                    </Row>
+                    {isLoading ? <Loader /> : null}
+                </tbody>
             </CoinTable>
         )
     }
     else {
-        return(
-            <h1>Loading...</h1>
-        )
+        return( <Loader /> )
     }
 }
